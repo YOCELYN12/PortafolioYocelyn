cd00c2afd01691dd5110daef6bb680bc2ca1b1b6
Yos12 <yocelynrivera9@gmail.com>
2025-01-23 23:28:31 -0600

    Experiencia agregada

--- PortafolioYocelyn/src/Pages/Descripcion.jsx
+++ PortafolioYocelyn/src/Pages/Descripcion.jsx
@@ -57,19 +57,74 @@ function Descripcion() {
       </div>
 
       <div className='ContenedorHabilidades'>
+
         <div className='cuadroSobreMi'>
 
           <h2 className='Letras'>Sobre mi</h2>
 
+          <div className='masPalabrasSobreMi' >
+
+            <p >Me adapto rápidamente a nuevas tecnologías, ofreciendo soluciones innovadoras y escalables que superan los desafíos
+              de las empresas en el entorno digital actual.</p>
+          </div>
+
         </div>
+
+        <div className='contenedorLineas>'>
+
+          <hr className='linea1' />
+
+          <p className='Experiencia'>Experiencia</p>
+
+          <hr className='linea2' />
+
+        </div>
+
+
+        <div className="experience-section">
+
+          
+          <div class="experience left">
+
+            <h3>CR Compus | Pasantía <span>Agosto 2022</span></h3>
+            <ul>
+              <li>Soporte técnico en sistemas tecnológicos.</li>
+              <li>Cableado estructurado.</li>
+              <li>Atención al cliente.</li>
+              <li>Gestión de inventario.</li>
+              <li>Publicidad.</li>
+              <li>Venta de equipos tecnológicos.</li>
+            </ul>
+          </div>
+
+          <div className="experience right">
+            <h3>SuppCenter | Telepráctica <span>Octubre – Diciembre 2023</span></h3>
+            <ul>
+              <li>Configuración del correo electrónico de Office 365.</li>
+              <li>Creación de máquina virtual usando Microsoft Hyper-V.</li>
+              <li>Configuración DNS del dominio.</li>
+              <li>Atención al cliente.</li>
+            </ul>
+          </div>
+
+          
+          <div className="experience center">
+            <h3>Deinsa Global | Pasantía | Enero 2025 - actual</h3>
+            <ul>
+              <li>Hosteo de páginas web.</li>
+              <li>Aprendizaje de nuevas tecnologías para la preparación y capacitación del equipo.</li>
+              <li>Automatizaciones con IA.</li>
+              <li>Participación en el proceso de selección de pasantes para el equipo.</li>
+              <li>Negociaciones para la venta de los servicios de la empresa.</li>
+            </ul>
+          </div>
+
+        </div>
+
       </div>
 
 
-
-
     </div>
-
-
 
   )
 }
